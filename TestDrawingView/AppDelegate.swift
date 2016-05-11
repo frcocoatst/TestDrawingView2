6c93deb6ed594be8a1c925c7eade4b1808a80432
frcocoatst <friedrichhaeupl@friedrichs-mbp.fritz.box>
2016-05-11 14:51:15 +0200

    Initial Commit

---
+++ TestDrawingView/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TestDrawingView
+//
+//  Created by Friedrich HAEUPL on 11.05.16.
+//  Copyright © 2016 Friedrich HAEUPL. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TestDrawingView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestDrawingView
+//
+//  Created by Friedrich HAEUPL on 11.05.16.
+//  Copyright © 2016 Friedrich HAEUPL. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
